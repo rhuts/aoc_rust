25fc99e2164b11bc8452529c688d9e54beb18195
Roman Huts <roman.h1234@gmail.com>
2021-12-24 18:00:16 -0500

    Complete day03

--- 2021/day03/src/main.rs
+++ 2021/day03/src/main.rs
@@ -1,5 +1,34 @@
-fn main() {
+fn get_most_common_bit<'a>(ratings: &'a mut Vec<&str>, pos: i32, is_most_common: bool) -> &'a str
+{
+    let mut count: i32 = 0;
+    for line in ratings
+    {
+        count += if &line[pos as usize..pos as usize +1] == "1" { 1 } else { -1 };
+    }
 
+    if is_most_common // oxygen rating
+    {
+        return if count >= 0 { "1" } else { "0" };
+    }
+    else // CO2 rating
+    {
+        return if count >= 0 { "0" } else { "1" };
+    }
+}
+
+fn remove_without_bit(ratings: &mut Vec<&str>, pos: i32, val: &str)
+{
+    for i in (0..ratings.len()).rev().step_by(1)
+    {
+        if i < ratings.len() && &ratings[i][pos as usize..pos as usize +1] != val
+        {
+            ratings.remove(i);
+        }
+    }
+}
+
+fn binary_diagnostic() -> (i32, isize)
+{
     // Part 1
     let file_name = "input.txt";
     
@@ -9,7 +38,7 @@ fn main() {
     let data: Vec<&str> = input.lines().collect();
     let num_cols = data[0].len();
 
-    for line in data
+    for line in &data // borrow 'data' to avoid moving
     {
         for i in 0..num_cols
         {
@@ -30,7 +59,7 @@ fn main() {
             epsilon += i32::pow(2, i as u32);
         }
     }
-    println!("{} x {} = {}", gamma, epsilon, gamma * epsilon);
+    let power_consumption = gamma * epsilon;
     
     // Part 2
     
@@ -40,10 +69,63 @@ fn main() {
     // determine the most common value in the current bit pos (start at 0)
     // keep only numbers with that bit in that position
     // if 0 and 1 are equally common, keep values with a 1 in that position
+    // repeat this process until you are left with just one number -> that is the rating
+    let mut o_ratings: Vec<&str> = data;
+    let mut pos: i32 = 0;
+    while o_ratings.len() != 1
+    {
+        if pos > 11 { break; }
+
+        let mut copy: Vec<&str> = o_ratings.to_owned();
+
+        let val: &str = get_most_common_bit(&mut copy, pos, true);
+        
+        remove_without_bit(&mut o_ratings, pos, val);
+
+        pos += 1;
+    }
     
     // find CO2 rating:
     // determing the least common value in the current bit position
     // keep only numbers with that bit in that position
     // if 0 and 1 are equally common, keep values with a 1 in that position
-    println!("{:?}", v);
+    // repeat this process until you are left with just one number -> that is the rating
+    pos = 0;
+    let input2 = std::fs::read_to_string(file_name).expect("file not found");
+    let data2: Vec<&str> = input2.lines().collect();
+    let mut co_ratings: Vec<&str> = data2;
+    while co_ratings.len() != 1
+    {
+        if pos > 11 { break; }
+
+        let mut copy: Vec<&str> = co_ratings.to_owned();
+
+        let val: &str = get_most_common_bit(&mut copy, pos, false);
+
+        remove_without_bit(&mut co_ratings, pos, val);
+
+        pos += 1;
+    }
+
+    let oxygen: isize = isize::from_str_radix(o_ratings[0], 2).unwrap();
+    let co2: isize = isize::from_str_radix(co_ratings[0], 2).unwrap();
+    let life_support = oxygen * co2;
+    
+    return (power_consumption, life_support);
+}
+
+fn main() {
+    let ans = binary_diagnostic();
+    println!("Power consumption is {}", ans.0);
+    println!("Life support rating is {}", ans.1);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn binary_diagnostic_test() {
+        assert_eq!(binary_diagnostic(), (2648450, 2845944));
+    }
 }
